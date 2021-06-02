80c6e49ca9aa80d6c518b170d91c2fcafe1d4bcf
sudarshan naidu <sudarshan.naidu@sts.in>
2021-06-03 00:27:28 +0530

    modifying code

--- cypress/integration/Framework.js
+++ cypress/integration/Framework.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
-import HomePage from "../integration/pageObjects/HomePage"
-import ProductsPage from "../integration/pageObjects/ProductsPage"
+import HomePage from "../support/pageObjects/HomePage"
+import ProductsPage from "../support/pageObjects/ProductsPage"
 describe('THsi is my second test Suite',function()
 {
     before (function()
@@ -17,7 +17,7 @@ it('THsi is my sample framework',function()
 {
     const homePage=new HomePage()
     const productsPage=new ProductsPage()
-cy.visit('https://rahulshettyacademy.com/angularpractice');
+cy.visit(Cypress.env('url'));
 homePage.getEditBox().type(this.data.name)
 homePage.getGender().select(this.data.gender)
 homePage.twoWayDataBinding().should('have.value',this.data.name)
@@ -29,6 +29,25 @@ this.data.productName.forEach(element => {
 });
 
 productsPage.checkoutPage().click()
+var total=0
+cy.get('tr td:nth-child(4) strong').each(($el,index,$list) =>
+{
+const amount=$el.text()
+var res=amount.split(" ")
+res=res[1].trim()
+total=Number(total)+Number(res)
+
+//cy.log($el.text())
+}).then(function(){
+    cy.log(total)
+})
+cy.get('h3 strong').then(function(element){
+    const amount=element.text()
+    var res=amount.split(" ")
+    var total1=res[1].trim()
+    expect(Number(total1)).equal(total)
+    
+})
 productsPage.ContinueCheckout().click()
 productsPage.DeliveryLocation().type('India')
 productsPage.SelectDeliveryLocationCountry().click()
--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -5,7 +5,7 @@ it('THsi is my first test case',function(){
 
     cy.visit('https://rahulshettyacademy.com/seleniumPractise');
     cy.get('.search-keyword').type('ca')
-    cy.get('.search-keyword').type('aa')
+    //cy.get('.search-keyword').type('aa')
     cy.wait(2000)
     cy.get('.product').should('have.length',5)
     //cy.get('.product:visible').should('have.length',4)
